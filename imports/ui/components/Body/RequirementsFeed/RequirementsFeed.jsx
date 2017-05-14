6b523ccc29e51affd1877c1d12e0090f82a30e34
Guillermo Pincay <guillermaster@gmail.com>
2017-05-14 13:24:19 -0500

    scrolling feed

--- imports/ui/components/Body/RequirementsFeed/RequirementsFeed.jsx
+++ imports/ui/components/Body/RequirementsFeed/RequirementsFeed.jsx
@@ -2,24 +2,18 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import FeedItem from './FeedItem';
-import InfiniteScroll from './infiniteScroll';
 
 class RequirementsFeed extends Component {
-    constructor(props){
-      super(props);
-      this.state = { limit: -1};
-    }
 
     renderFeedItems(){
       let items = [];
-      this.props.requirements.map((req) => {
-        items.push(<FeedItem title={req.title} content={req.content} />)
+      this.props.requirements.map((req, idx) => {
+        items.push(<FeedItem key={'req-'+idx} title={req.title} content={req.content} />)
       });
       return items;
     }
 
     render() {
-      console.log('limit: ', this.props.limit);
         return (
             <div>
                 { this.renderFeedItems() }
@@ -28,9 +22,9 @@ class RequirementsFeed extends Component {
     }
 }
 
-export default createContainer(() => {
+export default createContainer(({ max }) => {
   return {
-    requirements: Requirements.find({}, {sort:{createdOn: -1,}, }).fetch(),
+    requirements: Requirements.find({}, {sort:{createdOn: -1,}, limit:max}).fetch(),
     //limit: Session.get("feedItemsLimit")
   };
 }, RequirementsFeed);
--- imports/ui/pages/Dashboard/Dashboard.jsx
+++ imports/ui/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import RequirementsFeed from './../../components/Body/RequirementsFeed/RequirementsFeed';
 //import SummaryWidget from './../../components/Body/SummaryWidgets/SummaryWidget';
+//import RequirementsFeed from './../../containers/Meteor/RequirementsFeed';
+
+var lastScrollTop = 0;
 
 class Dashboard extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { requirementsLimit: 4 };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener('scroll', this.handleScroll.bind(this));
+  }
+
+  handleScroll(){
+    let scrollTop = event.srcElement.body.scrollTop;
+    if(lastScrollTop==0) lastScrollTop = scrollTop;
+    let height = event.srcElement.defaultView.screen.height;
+    let docHeight = event.srcElement.documentElement.offsetHeight;
+
+    if(scrollTop > lastScrollTop && (scrollTop+height) > docHeight){
+      this.setState({ requirementsLimit: this.state.requirementsLimit + 3 });
+    }
+    lastScrollTop = scrollTop;
+  }
 
   render() {
     return (
@@ -10,7 +38,7 @@ class Dashboard extends Component {
         <div className="row">
           <div className="col-md-8">
 
-              <RequirementsFeed />
+              <RequirementsFeed max={this.state.requirementsLimit} />
 
           </div>
           <div className="col-md-4">
