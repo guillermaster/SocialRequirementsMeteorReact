c4af9a5684f4661e505b45ac8b147be35781afec
guillermaster <guillermaster@gmail.com>
2017-05-22 22:42:27 -0500

    Modal pop-up sample onClick add button

--- imports/ui/components/Header/Header.jsx
+++ imports/ui/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Dropdown, DropdownMenu, DropdownItem } from 'reactstrap';
+//import { Dropdown, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class Header extends Component {
 
@@ -7,8 +8,10 @@ class Header extends Component {
     super(props);
 
     this.toggle = this.toggle.bind(this);
+    this.toggleNewRequirement = this.toggleNewRequirement.bind(this);
     this.state = {
-      dropdownOpen: false
+      dropdownOpen: false,
+      modalNewRequirement: false
     };
   }
 
@@ -16,6 +19,12 @@ class Header extends Component {
     e.preventDefault();
     this.setState({
       dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  toggleNewRequirement(){
+    this.setState({
+      modalNewRequirement: !this.state.modalNewRequirement
     });
   }
 
@@ -61,7 +70,7 @@ class Header extends Component {
           </ul>
           <ul className="nav navbar-nav ml-auto">
             <li className="nav-item d-md-down-none">
-              <a className="nav-link" href="#"><i className="icon-plus"></i></a>
+              <a className="nav-link" href="#" onClick={this.toggleNewRequirement}><i className="icon-plus"></i></a>
             </li>
             <li className="nav-item d-md-down-none">
               <a className="nav-link" href="#"><i className="icon-magnifier"></i></a>
@@ -86,6 +95,17 @@ class Header extends Component {
             </a>
           </li>
           </ul>
+
+          <Modal isOpen={this.state.modalNewRequirement} toggle={this.toggleNewRequirement} className={'modal-lg ' + this.props.className}>
+                  <ModalHeader toggle={this.toggleNewRequirement}>Modal title</ModalHeader>
+                  <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="primary" onClick={this.toggleNewRequirement}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={this.toggleNewRequirement}>Cancel</Button>
+                  </ModalFooter>
+                </Modal>
       </header>
     )
   }
